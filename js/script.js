c5851ee7e31d358f20811873dee07d426221559d
Doroga80 <rusdorog@yandex.by>
2024-06-06 08:50:13 +0300

    Внесли изменения в файл js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,59 @@
-const citySelect = document.querySelector('#city');
-const cityChoices = new Choices(citySelect, {
-    searchEnabled: false,
-    itemSelectText: " ",
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
+
+
+
+const getDATA = async (url, cbSuccess, cdError) => {
+    try {
+        const Response = await fetch(url);
+        const data = await Response.json();
+        cbSuccess(data)
+    } catch (err) {
+        cdError(err)
+    }
+}
+
+const init = () => {
+    const citySelect = document.querySelector("#city");
+    const cityChoices = new Choices(citySelect, {
+        itemSelectText: "",
+});
+
+    getDATA(
+        `${API_URL}${LOCATION_URL}`,
+        (locationData) => {
+            const locations = locationData.map(location => ({
+                value: location,
+            }));
+            cityChoices.setChoices(locations, "value","label", true,
+            );
+        }, 
+        (err) => {
+            console.log(err);
+        },
+    );
+};
+
+init();
+
+// getDATA(
+//     API_URL + LOCATION_URL, 
+//     (data) => {
+//         console.log(data)
+//     }, 
+//     (err) => {
+//         console.log(err);
+//     },
+// );
+
+fetch(API_URL + LOCATION_URL) 
+.then((Response) => {
+    return Response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((err) => {
+    console.log(err);
 });
