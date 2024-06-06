52bd53537d09cc77383939961e716269ee540037
Doroga80 <rusdorog@yandex.by>
2024-06-06 13:53:40 +0300

    Внесли изменения в скрипт

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const API_URL = "https://workspace-methed.vercel.app/";
 const LOCATION_URL = "api/locations";
+const VACANCY_URL = "api/locations";
 
+const cardsList = document.querySelector('.cards__list');
 
 
 
@@ -12,9 +14,45 @@ const getDATA = async (url, cbSuccess, cdError) => {
     } catch (err) {
         cdError(err)
     }
-}
+};
+
+const createCard = vacancy => `
+    <article class="vacancy" tabindex="0">
+        <img class="vacancy__logo" src="img/yadro.svg" alt="Логотип компании YDRO">
+
+            <p class="vacancy__company">YADRO</p>
+
+            <h3 class="vacancy__title">Инженер</h3>
+
+            <ul class="vacancy__fields">
+                <li class="vacancy__fields">от 150 000₽</li>
+                <li class="vacancy__fields">полная занятость</li>
+                <li class="vacancy__fields">офис</li>
+                <li class="vacancy__fields">опыт от 3-х лет</li>
+           </ul>
+    </article>
+`;
+
+const createCards = (data) =>
+    data.vacancies.map(vacancy => {
+        const li = document.createElement('li');
+        li.classList.add('cards__item');
+        li.insertAdjacentHTML('beforeend', createCard(vacancy));
+        return li;
+    })
+
+const renderVacancy = data => {
+    cardsList.textContent = "";
+    const cards = createCards(data); 
+    cardsList.append(...cards);
+};
+
+const renderError = err => {
+    console.warn(err);
+};
 
 const init = () => {
+    // select city
     const citySelect = document.querySelector("#city");
     const cityChoices = new Choices(citySelect, {
         itemSelectText: "",
@@ -33,6 +71,12 @@ const init = () => {
             console.log(err);
         },
     );
+
+    // cards
+
+    const url = new URL(`${API_URL}${LOCATION_URL}`);
+
+    getDATA(url, renderVacancy, renderError);
 };
 
 init();
